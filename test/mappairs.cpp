dd387e744d5dfd74b5f55908256a9316a90cbaff
Mike <mike@pc-249.home>
2024-12-05 17:54:55 +0100

    Using makepairs data, it is possible to update nodes in the map container

--- test/mappairs.cpp
+++ test/mappairs.cpp
@@ -15,23 +15,53 @@ typedef std::pair<ULL,ULL> Coords;
 
 int main(int argc, char **argv)
 {
-	std::multimap<Coords,std::string> pmap;
-	std::multimap<Coords,std::string>::iterator imap;
+	std::multimap<Coords,std::vector<ULL>> pmap;
+	std::multimap<Coords,std::vector<ULL>>::iterator imap;
 	Coords c1{1,2};
-	Coords c2{2,1};
-	Coords c3{2,1};
+	Coords c2{10,20};
+	Coords c3{10,20};
 
-	imap = pmap.insert({c1,"node 1"});
-	cout << (imap->first).first<< "," << (imap->first).second << " " << (imap->second) << endl;
+	imap = pmap.insert({c1,{1,2}});
+	cout << "Coords: " << (imap->first).first<< "," << (imap->first).second << endl;
+	for(auto d : (imap->second)) cout << d << " ";
+	cout << endl;
 
-	imap = pmap.insert({c2,"node 2"});
-	cout << (imap->first).first << "," << (imap->first).second << " " << (imap->second) << endl;
+	imap = pmap.insert({c2,{10,20}});
+	cout << "Coords: " << (imap->first).first << "," << (imap->first).second << endl;
+	for(auto d : (imap->second)) cout << d << " ";
+	cout << endl;
 
 	cout << "Attempting search" << endl;
-	imap = pmap.find(c2);
-	if(imap != pmap.end())
-			cout << (imap->first).first << "," << (imap->first).second << " " << (imap->second) << endl;
+	imap = pmap.find(c3);
+	if(imap != pmap.end()) {
+			cout << "Coords: " << (imap->first).first << "," << (imap->first).second << endl;
+			for(auto d : (imap->second)) cout << d << " ";
+	} else {
+		cout << "Search failed." << endl;
+	}
+	cout << endl;
 
+	cout << "modifying vector" << endl;
+	imap = pmap.find(c3);
+	if(imap != pmap.end()) {
+			// modify the vector
+			for(auto i = (imap->second).begin(); i !=(imap->second).end(); ++i ) *i *= 13;
+			cout << "Coords: " << (imap->first).first << "," << (imap->first).second << endl;
+			for(auto d : (imap->second)) cout << d << " ";
+	} else {
+		cout << "Search failed." << endl;
+	}
+	cout << endl;
+	
+	cout << "Attempting confirmation" << endl;
+	imap = pmap.find(c3);
+	if(imap != pmap.end()) {
+			cout << "Coords: " << (imap->first).first << "," << (imap->first).second << endl;
+			for(auto d : (imap->second)) cout << d << " ";
+	} else {
+		cout << "Search failed." << endl;
+	}
+	cout << endl;
 
 	return 0;
 }
