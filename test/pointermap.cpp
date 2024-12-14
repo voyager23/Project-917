53ab23ed0e4b366738a126bfbf181789351d5ef3
Mike <Mike@Aorus_F39>
2024-12-14 17:33:41 +0100

    testing/pointermap restore point only. Output incorret

--- test/pointermap.cpp
+++ test/pointermap.cpp
@@ -9,9 +9,8 @@ pointermap.cpp
 #include <climits>
 
 using namespace std;
-typedef int64_t LL;
-typedef std::pair<LL,LL > Coords;
-typedef std::pair<LL,LL > SubValues;
+typedef std::pair<long long,long long > Coords;
+typedef std::pair<long long,long long > SubValues;
 class Node;	//Forward Declaration
 
 // --------------------class declaration----------------------
@@ -20,20 +19,20 @@ public:
 	// Data
 	Coords coords;
 	SubValues aibj;	// pair of components of local value - derived from Sn = (Sn-1)^2 mod 998388889
-	LL local_value;
-	// LL  sum_path = 999999999;
-	static const LL M = 3;	// matrix dimension
-	static const LL mod = 21;	// modulus
+	long long local_value;
+	// long long  sum_path = 999999999;
+	static const long long M = 3;	// matrix dimension
+	static const long long mod = 21;	// modulus
 
-	LL fromN = mod * 2;	// default values
-	LL fromW = mod * 2;
-	LL minimum_path = 0;	// minimum of fromN and fromW
+	long long fromN = mod * 2;	// default values
+	long long fromW = mod * 2;
+	long long minimum_path = 0;	// minimum of fromN and fromW
 
 	// Functions
 	Node();
-	pair<Node*,Node*> neighbours(std::map<Coords, Node*>& id_node_map)const;
+	pair<Node*,Node*> neighbours(std::map<Coords, Node*>& id_node_map);
 	void prt_node() const;	// const tells compiler nothing will change inside this function
-	LL  move_sn_2places(LL sn)const;
+	long long  move_sn_2places(long long sn);
 	bool goal() const;
 };
 
@@ -46,7 +45,7 @@ Node::Node(){	// constructor
 	fromW = LLONG_MAX;
 }
 
-pair<Node*,Node*> Node::neighbours(std::map<Coords, Node*>& id_node_map)const{
+pair<Node*,Node*> Node::neighbours(std::map<Coords, Node*>& id_node_map){
 	// M is matrix size
 	// Do sanity checks based on current coords
 	// Use current coords and values for min_path, ai and bj.
@@ -58,9 +57,11 @@ pair<Node*,Node*> Node::neighbours(std::map<Coords, Node*>& id_node_map)const{
 		Node* d = new Node;
 		// new coords
 		d->coords = {coords.first+1, coords.second};
+
 		// new aibj
 		d->aibj = {move_sn_2places(aibj.first), aibj.second};	// new ai (row)
-		// set local valur
+		
+		// set local value
 		d->local_value = aibj.first + aibj.second;
 		// fromNorth = this local_value + parent local value
 		d->fromN = d->aibj.first + d->aibj.second + local_value;
@@ -80,8 +81,10 @@ pair<Node*,Node*> Node::neighbours(std::map<Coords, Node*>& id_node_map)const{
 		Node* r = new Node;
 		// new coords
 		r->coords = {coords.first, coords.second+1};
+
 		// new aibj
 		r->aibj = {aibj.first, move_sn_2places(aibj.second)};	// new bj (col)
+		
 		// set new local_value
 		r->local_value = r->aibj.first + r->aibj.second;
 		// fromW = new local_value + this local value
@@ -101,9 +104,9 @@ pair<Node*,Node*> Node::neighbours(std::map<Coords, Node*>& id_node_map)const{
 	return dnrt;
 }
 
-LL  Node::move_sn_2places(LL  sn)const{
+long long  Node::move_sn_2places(long long sn){
 	// move sn 2 places along the Sn sequence
-	LL  sm = (sn*sn)%mod;
+	long long  sm = (sn*sn)%mod;
 	return ((sm*sm)%mod);
 }
 
@@ -111,7 +114,7 @@ void Node::prt_node() const { // const tells compiler nothing will change inside
 	cout << "Node {" << coords.first << "," << coords.second << "}" << endl;
 	cout << "ai:" << aibj.first << " bj:" << aibj.second << endl;
 	cout << "node_value:" << local_value << endl;
-	cout << "minimum_path:" << minimum_path;
+	cout << "minimum_path:" << minimum_path << endl;
 	cout << endl;
 }
 
@@ -125,8 +128,8 @@ bool Node::goal() const{
 int main(int argc, char **argv)
 {
 
-	std::multimap<LL, Node*> min_cost_map;	// map multiple minimum cost path values to a node pointer
-	std::multimap<LL, Node*>::iterator mincost_i;
+	std::multimap<long long, Node*> min_cost_map;	// map multiple minimum cost path values to a node pointer
+	std::multimap<long long, Node*>::iterator mincost_i;
 
 	std::map<Coords, Node*> id_node_map;	// map unique key coordinates to a Node pointeer
 	std::map<Coords, Node*>::iterator idnode_i;
@@ -144,7 +147,7 @@ int main(int argc, char **argv)
 	// -----LOOP-----
 
 	while(1){
-		pair<Node*,Node*> adjacent = working ->neighbours(id_node_map);
+		pair<Node*,Node*> adjacent = working -> neighbours(id_node_map);
 
 		if(((adjacent.first)==NULL) and ((adjacent.second)==NULL)){
 			cout << "\nGoal Node reached\n";
@@ -161,9 +164,9 @@ int main(int argc, char **argv)
 			min_cost_map.insert({(adjacent.second)->minimum_path, adjacent.second});
 			id_node_map.insert({(adjacent.second)->coords, adjacent.second});
 		}
-					// cout << "\nbefore:\n";
-					// for(auto m : min_cost_map) (m.second)->prt_node(); // DEBUG only
-					// cout << "complete.\n";
+					cout << "\nmin_cost_map before:\n";
+					for(auto m : min_cost_map) (m.second)->prt_node(); // DEBUG only
+					cout << "min_cost_map complete.\n";
 
 		// Remove working node from two maps
 		auto foo = min_cost_map.find(working->minimum_path);
@@ -182,12 +185,14 @@ int main(int argc, char **argv)
 			//cout << "id_node_map.find() failed" << endl;
 		}
 
-					// cout << "\nafter:\n";
-					// for(auto m : min_cost_map) (m.second)->prt_node(); // DEBUG only
-					// cout << "complete.\n";
+					cout << "\nafter:\n";
+					for(auto m : min_cost_map) (m.second)->prt_node(); // DEBUG only
+					cout << "complete.\n";
 		
 		// update the working pointer to top of min_cost_map
 		working = (min_cost_map.begin())->second;
+
+		break;
 	}
 
 
