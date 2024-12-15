3152c7e5035f981b5710d65d5eac3e6d67f1bfc2
Mike <mike@X1carbon>
2024-12-15 13:05:42 +0100

    branch switch

--- test/QRmod21.cpp
+++ test/QRmod21.cpp
@@ -7,30 +7,61 @@
 #include <vector>
 #include <cstdint>
 #include <cmath>
+#include <set>
 
 using namespace std;
+set<int> seq;
+
+void prt_set(set<int> seq){
+	for(auto n : seq) cout << n << " ";
+	cout << endl;
+}	
 
 int main(int argc, char **argv)
 {
 
-	const int mod = (21);
+	const int mod = (29*41);
 	const int mid = (mod / 2);	//integer division
-	int n=0, m=0;
-	
-	// cout << min(1,2) << endl;
-	// exit(0);
+	int seed, Sa, Sb;
 
-	cout << "Quadratic residues modulo " << mod << endl << endl;
-	//cout << n << ")\t" << m << "\t" << endl;
+	int max_cycle = 0;
 
-	for(auto n = 0; n != 2*mod+1; ++n){
-		
-		m = (n*n)%mod;
-		cout << n << ")\t";
-		if((n%2)==0) cout << "\t";
-		cout << m << endl;
-		if ((n%mid)==0) cout << "....................\n";
-	}
-	
+	for(seed = 36; seed != 37; ++seed){
+		Sa = seed;
+		seq.clear();
+		auto result = seq.insert(Sa);
+		if((result.second)==false){
+			cout << "insert seed error" << endl;
+			exit(1);
+		}
+		while(1){
+			Sb = (Sa*Sa) % mod;
+			auto result = seq.insert(Sb);
+			if((result.second)==false){
+				cout << "seed " << seed << " cycle " << seq.size() << endl;
+				if(seq.size() > max_cycle) max_cycle = seq.size();
+				prt_set(seq);
+				break;
+			} //if
+			Sa = Sb;
+		} //whilw
+	} //for	
+	cout << "\nComplete. max_cycle = " << max_cycle << endl;
 	return 0;
 }
+
+
+	// // cout << min(1,2) << endl;
+	// // exit(0);
+
+	// cout << "Quadratic residues modulo " << mod << endl << endl;
+	// //cout << n << ")\t" << m << "\t" << endl;
+
+	// for(auto n = 0; n != 2*mod+1; ++n){
+		
+	// 	m = (n*n)%mod;
+	// 	cout << n << ")\t";
+	// 	if((n%2)==0) cout << "\t";
+	// 	cout << m << endl;
+	// 	if ((n%mid)==0) cout << "....................\n";
+	// }
